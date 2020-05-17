c5d859b4c4af19fa8faa10cdf3e228e83d4e0044
camilo <jcamilovelandiab@gmail.com>
2020-05-16 22:44:13 -0500

    refactorizing item information activity

--- app/src/main/java/com/app/gorent/ui/activities/item_information/ItemInformationActivity.java
+++ app/src/main/java/com/app/gorent/ui/activities/item_information/ItemInformationActivity.java
@@ -2,6 +2,7 @@ package com.app.gorent.ui.activities.item_information;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.app.Activity;
@@ -10,36 +11,60 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.app.gorent.R;
+import com.app.gorent.data.model.Item;
+import com.app.gorent.ui.activities.new_item.ItemFormActivity;
 import com.app.gorent.ui.viewmodel.ViewModelFactory;
+import com.app.gorent.utils.ItemQueryResult;
 
 public class ItemInformationActivity extends AppCompatActivity {
 
     ItemInformationViewModel itemInformationViewModel;
 
+    EditText et_item_name, et_item_description, et_item_price;
+    ImageView iv_item_picture;
+    Spinner sp_category;
     Button btn_update, btn_cancel;
+    ProgressBar pg_loading;
+
     LinearLayout layout_update_buttons;
     boolean updateMode = false;
+
+    Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_information);
-        itemInformationViewModel = ViewModelProviders.of(this, new ViewModelFactory()).get(ItemInformationViewModel.class);
 
+        itemInformationViewModel = ViewModelProviders.of(this, new ViewModelFactory()).get(ItemInformationViewModel.class);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.title_activity_item_info);
         connectModelWithView();
         configureBtnCancel();
+        prepareItemQueryObserver();
+        bundle = this.getIntent().getExtras();
+        assert bundle != null;
+        itemInformationViewModel.findItemById(bundle.getLong("itemId"));
     }
 
     private void connectModelWithView() {
-        btn_update =findViewById(R.id.item_information_btn_update);
-        btn_cancel = findViewById(R.id.item_information_btn_cancel);
-        layout_update_buttons = findViewById(R.id.item_information_layout_update_buttons);
+        btn_update =findViewById(R.id.item_info_btn_update);
+        btn_cancel = findViewById(R.id.item_info_btn_cancel);
+        layout_update_buttons = findViewById(R.id.item_info_layout_update_buttons);
+        pg_loading = findViewById(R.id.item_info_pg_loading);
+        et_item_name = findViewById(R.id.item_info_et_item_name);
+        et_item_description = findViewById(R.id.item_info_et_item_description);
+        et_item_price = findViewById(R.id.item_info_et_item_price);
+        iv_item_picture = findViewById(R.id.item_info_iv_item_picture);
+        sp_category = findViewById(R.id.item_info_sp_category);
     }
 
     @Override
@@ -88,6 +113,26 @@ public class ItemInformationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 deactivateUpdate();
+            }
+        });
+    }
+
+    private void prepareItemQueryObserver(){
+        itemInformationViewModel.getItemQueryResult().observe(this, new Observer<ItemQueryResult>() {
+            @Override
+            public void onChanged(ItemQueryResult itemQueryResult) {
+                if(itemQueryResult==null) return;
+                pg_loading.setVisibility(View.GONE);
+                if(itemQueryResult.getError()!=null){
+                    Toast.makeText(ItemInformationActivity.this, itemQueryResult.getError(), Toast.LENGTH_SHORT).show();
+                }
+                if(itemQueryResult.getItem()!=null){
+                    Item item = itemQueryResult.getItem();
+                    et_item_name.setText(item.getName());
+                    et_item_description.setText(item.getDescription());
+                    et_item_price.setText(item.getPrice().toString());
+                    //...... image, category....
+                }
             }
         });
     }
--- app/src/main/java/com/app/gorent/ui/activities/item_information/ItemInformationViewModel.java
+++ app/src/main/java/com/app/gorent/ui/activities/item_information/ItemInformationViewModel.java
@@ -1,21 +1,28 @@
 package com.app.gorent.ui.activities.item_information;
 
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.app.gorent.data.model.Item;
 import com.app.gorent.data.repositories.ItemRepository;
+import com.app.gorent.utils.ItemQueryResult;
 
 public class ItemInformationViewModel extends ViewModel {
 
     ItemRepository itemRepository;
+    MutableLiveData<ItemQueryResult> itemQueryResult = new MutableLiveData<>();
 
     public ItemInformationViewModel(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
     }
 
-    public Item findItemById(Long id){
-        return itemRepository.getItemById(id);
+    public MutableLiveData<ItemQueryResult> getItemQueryResult() {
+        return itemQueryResult;
+    }
+
+    public void findItemById(Long id){
+        itemRepository.getItemById(id, itemQueryResult);
     }
 
 }
