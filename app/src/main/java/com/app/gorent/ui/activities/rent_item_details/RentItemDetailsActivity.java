b71caf5052e5490d454e95ac0126e4e790ef8023
camilo <jcamilovelandiab@gmail.com>
2020-05-16 23:41:34 -0500

    refactorizing rent item details activity

--- app/src/main/java/com/app/gorent/ui/activities/rent_item_details/RentItemDetailsActivity.java
+++ app/src/main/java/com/app/gorent/ui/activities/rent_item_details/RentItemDetailsActivity.java
@@ -1,20 +1,27 @@
 package com.app.gorent.ui.activities.rent_item_details;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.gorent.R;
 import com.app.gorent.data.model.Item;
 import com.app.gorent.ui.activities.rental_form.RentalFormActivity;
 import com.app.gorent.ui.viewmodel.ViewModelFactory;
+import com.app.gorent.utils.ItemQueryResult;
 
 public class RentItemDetailsActivity extends AppCompatActivity {
 
@@ -22,7 +29,7 @@ public class RentItemDetailsActivity extends AppCompatActivity {
     TextView tv_item_information;
     ImageView iv_item_picture;
     Button btn_rent;
-    Bundle bundle;
+    Long itemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +41,9 @@ public class RentItemDetailsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.title_activity_item_info);
 
-        bundle = getIntent().getExtras();
-        retrieveItemInformation(bundle.getLong("itemId"));
-        configureBtnRent();
-    }
-
-    private void connectViewWithModel(){
-        iv_item_picture = findViewById(R.id.item_info_iv_item_picture);
-        tv_item_information = findViewById(R.id.item_info_tv_item_information);
-        btn_rent = findViewById(R.id.item_info_btn_rent);
-    }
-
-    private void configureBtnRent(){
-        btn_rent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(RentItemDetailsActivity.this, RentalFormActivity.class);
-                intent.putExtra("itemId", rentItemDetailsViewModel.getItem().getId());
-                startActivity(intent);
-            }
-        });
+        itemId = this.getIntent().getExtras().getLong("itemId");
+        rentItemDetailsViewModel.retrieveItemById(itemId);
+        configureItemObserver();
     }
 
     @Override
@@ -63,18 +53,82 @@ public class RentItemDetailsActivity extends AppCompatActivity {
         return true;
     }
 
-    private void retrieveItemInformation(Long itemId) {
-        Item item = rentItemDetailsViewModel.retrieveItemById(itemId);
-        tv_item_information.setText(item.toString());
-        if(item.getCategory().getName().toLowerCase().equals("houses")){
-            iv_item_picture.setImageDrawable(getResources().getDrawable(R.drawable.houses));
-        }else if(item.getCategory().getName().toLowerCase().equals("cars")){
-            iv_item_picture.setImageDrawable(getResources().getDrawable(R.drawable.cars));
-        }else if(item.getCategory().getName().toLowerCase().equals("pianos")){
-            iv_item_picture.setImageDrawable(getResources().getDrawable(R.drawable.pianos));
-        }else if(item.getCategory().getName().toLowerCase().equals("laptops")){
-            iv_item_picture.setImageDrawable(getResources().getDrawable(R.drawable.laptops));
-        }
+    private void connectViewWithModel(){
+        iv_item_picture = findViewById(R.id.item_info_iv_item_picture);
+        tv_item_information = findViewById(R.id.item_info_tv_item_information);
+        btn_rent = findViewById(R.id.item_info_btn_rent);
+    }
+
+    private void configureItemObserver() {
+        rentItemDetailsViewModel.getItemQueryResult().observe(this, new Observer<ItemQueryResult>() {
+            @Override
+            public void onChanged(ItemQueryResult itemQueryResult) {
+                if(itemQueryResult==null) return;
+
+                if(itemQueryResult.getError()!=null){
+                    showErrorMessage(getString(itemQueryResult.getError()));
+                }
+                if(itemQueryResult.getItem()!=null){
+                    Item item = itemQueryResult.getItem();
+                    tv_item_information.setText(item.toString());
+                    if(item.getCategory().getName().toLowerCase().equals("houses")){
+                        iv_item_picture.setImageDrawable(getResources().getDrawable(R.drawable.houses));
+                    }else if(item.getCategory().getName().toLowerCase().equals("cars")){
+                        iv_item_picture.setImageDrawable(getResources().getDrawable(R.drawable.cars));
+                    }else if(item.getCategory().getName().toLowerCase().equals("pianos")){
+                        iv_item_picture.setImageDrawable(getResources().getDrawable(R.drawable.pianos));
+                    }else if(item.getCategory().getName().toLowerCase().equals("laptops")){
+                        iv_item_picture.setImageDrawable(getResources().getDrawable(R.drawable.laptops));
+                    }
+                    configureBtnRent();
+                }
+
+            }
+        });
+    }
+
+    private void configureBtnRent(){
+        btn_rent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RentItemDetailsActivity.this, RentalFormActivity.class);
+                intent.putExtra("itemId", itemId);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void showErrorMessage(final String errorMsg){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Toast toast = Toast.makeText(RentItemDetailsActivity.this, errorMsg, Toast.LENGTH_LONG);
+                View view = toast.getView();
+                //Gets the actual oval background of the Toast then sets the colour filter
+                view.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                //Gets the TextView from the Toast so it can be edited
+                TextView text = view.findViewById(android.R.id.message);
+                text.setGravity(Gravity.CENTER);
+                text.setTextColor(Color.WHITE);
+                toast.show();
+            }
+        }, 2500);
+    }
+
+    private void showMessage(final String msg){
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast toast = Toast.makeText(RentItemDetailsActivity.this, msg, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0,0);
+                View view = toast.getView();
+                //Gets the actual oval background of the Toast then sets the colour filter
+                view.getBackground().setColorFilter(Color.parseColor("#03DAC5"), PorterDuff.Mode.SRC_IN);
+                //Gets the TextView from the Toast so it can be edited
+                TextView text = view.findViewById(android.R.id.message);
+                text.setTextColor(Color.BLACK);
+                toast.show();
+            }
+        });
     }
 
 
--- app/src/main/java/com/app/gorent/ui/activities/rent_item_details/RentItemDetailsViewModel.java
+++ app/src/main/java/com/app/gorent/ui/activities/rent_item_details/RentItemDetailsViewModel.java
@@ -1,26 +1,27 @@
 package com.app.gorent.ui.activities.rent_item_details;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.app.gorent.data.model.Item;
 import com.app.gorent.data.repositories.ItemRepository;
+import com.app.gorent.utils.ItemQueryResult;
 
 public class RentItemDetailsViewModel extends ViewModel {
 
+    private MutableLiveData<ItemQueryResult> itemQueryResult = new MutableLiveData<>();
     private ItemRepository itemRepository;
-    private Item item;
 
     public RentItemDetailsViewModel(ItemRepository itemRepository){
         this.itemRepository = itemRepository;
     }
 
-    public Item retrieveItemById(Long id){
-        this.item = itemRepository.getItemById(id);
-        return item;
+    public MutableLiveData<ItemQueryResult> getItemQueryResult() {
+        return itemQueryResult;
     }
 
-    public Item getItem(){
-        return item;
+    public void retrieveItemById(Long id){
+        itemRepository.getItemById(id, itemQueryResult);
     }
 
 }
